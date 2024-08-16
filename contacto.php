09f4bedb1b5a6dbaee0030b392aa25ca21b35ba1
Morris Joyner <92999301+mjoynermx@users.noreply.github.com>
2024-08-16 12:28:49 -0600

    Add files via upload

--- contacto.php
+++ contacto.php
@@ -1,18 +1,54 @@
 <?php
-if(isset($_POST['submit'])){
-    $name = $_POST['nombre'];
-    $mailForm = $_POST['mail'];
-    $tel = $_POST['tel'];
-    $msj = $_POST['mensaje'];
+ $name = $_POST['nombre'];
+ $mailForm = $_POST['mail'];
+ $tel = $_POST['tel'];
+ $msj = $_POST['mensaje'];
 
-    $mailTo = "ljuarez@ikeasistencia.com";
-    $headers = "De: ".$mailFrom;
-    $text = "Tiene un mensaje de: ".$nombre;
-    $text1 = "Teléfono de contacto: ".$tel;
-    $text2 = "Mensaje:".$msj;
+ $mailTo = "ljuarez@ikeasistencia.com";
+ $headers = "De: ".$mailFrom;
+ $text = "Tiene un mensaje de: ".$nombre;
+ $text1 = "Teléfono de contacto: ".$tel;
+ $text2 = "Mensaje:".$msj;
+// Varios destinatarios
+$para  = 'ljuarez@ikeasistencia.com' . ', '; // atención a la coma
+//$para .= 'wez@example.com';
 
-    mail($mailTo, $text, $text1, $text2, $headers );
-}
+// título
+$título = 'Recordatorio de cumpleaños para Agosto';
 
+// mensaje
+$mensaje = '
+<html>
+<head>
+  <title>Recordatorio de cumpleaños para Agosto</title>
+</head>
+<body>
+  <p>¡Estos son los cumpleaños para Agosto!</p>
+  <table>
+    <tr>
+      <th>Quien</th><th>Día</th><th>Mes</th><th>Año</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3</td><td>Agosto</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17</td><td>Agosto</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
 
+// Para enviar un correo HTML, debe establecerse la cabecera Content-type
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Cabeceras adicionales
+$cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+$cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
+$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
+$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+
+// Enviarlo
+mail($mailTo, $text, $text1, $text2, $headers, $cabeceras);
 ?>
